8ca3e5a0bbc4ff16263668650617cff6ee396cd4
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-02 14:47:04 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- M4/p1/vowels_counter.py
+++ M4/p1/vowels_counter.py
@@ -1,12 +1,11 @@
-
-"""Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:"""
-
+"""This program evaluates the numbers of vowels in a string."""
 def main():
-	s = input("enter any string")
-num = 0
-for letter in s:
-    if letter in ('a', 'e', 'i','o', 'u', 'A', 'E', 'I','O', 'U') :
-        num += 1
-print('Number of vowels: ' + str(num))
-if __name__== "__main__":
-	main()
+    """main function"""
+    str_1 = input()
+    num = 0
+    for letter in str_1:
+        if letter in ('a', 'e', 'i', 'o', 'u'):
+            num = num+1
+    print(num)
+if __name__ == "__main__":
+    main()
