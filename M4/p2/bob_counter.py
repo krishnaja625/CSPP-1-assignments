7c09260e5dea83708d5d703383159dfb243b28b2
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-02 17:58:39 +0530

    " -> 5 of 5 passed. pylint: 8.46/10 "

--- M4/p2/bob_counter.py
+++ M4/p2/bob_counter.py
@@ -6,7 +6,7 @@ def main():
     str2 = 'bob'
     cnt = 0
     for i in range(len(str1)-2):
-        if str1[i]+str1[i+1]+str1[i+2] == str2:
+        if str2 == str1[i]+str1[i+1]+str1[i+2]:
             cnt = cnt+1
     print(cnt)
 if __name__ == "__main__":
---
+++ M4/p3/longest_substring.py
@@ -0,0 +1,15 @@
+def main():
+    """Count the occurence of the substring in a string"""
+    str_ = input()
+    max_str = ''
+    for i in range(0, len(str_)):
+        sub_str = str_[i]
+        while i + 1 < len(str_) and str_[i] <= str_[i+1]:
+            i += 1
+            sub_str += str_[i]
+        if len(sub_str) > len(max_str):
+            max_str = sub_str
+    print(max_str)
+
+if __name__ == "__main__":
+    main()
