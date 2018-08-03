e6a832222b716a4fa8d34e6ffe696f55c3c15e75
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-03 16:15:41 +0530

    " -> 0 of 4 passed. pylint: 0.00/10 "

---
+++ M5/GuessMyNumber/guess_my_number.py
@@ -0,0 +1,17 @@
+"""Guess My Number Exercise"""
+
+def main():
+	"""cube root"""
+	s_1 = int(input("enter any number"))
+	s_2 = abs(s_1)
+	for guess in range(1, S_1+1)
+        if(guess**3 >= s_1)
+            break
+        if(guess**3 = s_1)
+            print("cube root is possible")
+        else
+            print("cube root is not possible")
+	
+if __name__== "__main__":
+	main()
+	
---
+++ M5/p1/perfect_cube.py
@@ -0,0 +1,26 @@
+# Write a python program to find if the given number is a perfect cube or not
+# using guess and check algorithm
+
+# testcase 1
+# Input: 24389
+# Output: 24389 is a perfect cube
+
+# testcase 2
+# Input: 21950
+# Output: 21950 is not a perfect cube
+
+def main():
+    """cube root"""
+    s_1 = int(input("enter any number"))
+    s_2 = abs(s_1)
+    for guess in range(1, S_1+1):
+        if(guess**3 >= s_1):
+            break
+        if(guess**3 == s_1):
+            print("cube root is possible")
+        else:
+            print("cube root is not possible")
+	
+if __name__== "__main__":
+    main()
+	
