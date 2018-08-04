76810b2d328412079ee996fc7065e00b179fdd7f
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-04 17:19:49 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

--- M5/p2/square_root.py
+++ M5/p2/square_root.py
@@ -1,7 +1,7 @@
 '''This program evaluates the square root of a number.'''
 def main():
     '''Main function.'''
-    num_1 = input() 
+    num_1 = int(input())
     epsilon = 0.01
     step = 0.1
     guess = 0
