b703cd42e0b4b18a4d0b85be095ed14f87753831
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-04 11:32:24 +0530

    " -> 7 of 7 passed. pylint: 9.44/10 "

--- m6/p1/fizz_buzz.py
+++ m6/p1/fizz_buzz.py
@@ -5,7 +5,7 @@ For each multiple of 5, print "Buzz" instead of the number.
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 '''
 def main():
-    '''Read number from the input, store it in variable num.'''
+    '''Read'''
 N = int(input())
 for i in range(1, N+1):
     if i%3 == 0 and i%5 == 0:
