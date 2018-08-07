bccd591748b9dd80b24be804fa4ac0dfc67090a0
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-07 19:59:47 +0530

    act10

--- m8/Is In Exercise/is_in.py
+++ m8/Is In Exercise/is_in.py
@@ -1,26 +1,27 @@
-# Exercise: Is In
-# Write a Python function, isIn(char, aStr), that takes in two arguments a character and String and returns the isIn(char, aStr) which retuns a boolean value.
+'''Exercise: Is In'''
+# Write a Python function, isIn(char, aStr), that takes in two arguments a character and String
+#and returns the isIn(char, aStr) which retuns a boolean value.
 
 # This function takes in two arguments character and String and returns one boolean value.
-
-def isIn(char, aStr):
+def isin(char, astr, i_1):
     '''
     char: a single character
     aStr: an alphabetized string
-    
     returns: True if char is in aStr; False otherwise
     '''
     # Your code here
-    if char == astr[0]:
+    if char == astr[i_1]:
         return True
-    elif len(astr) == 1:
-        return False
-    else:
-        return isIn(char, astr[1:])
-
+    if i_1 < len(astr)-1:
+        if char != astr[i_1]:
+            return isin(char, astr, i_1+1)
+    return False
 def main():
+    '''Main Function for char checker'''
     data = input()
     data = data.split()
-    print(isIn((data[0][0]), data[1]))
+    a_1 = 0
+    print(isin((data[0]), data[1], a_1))
 if __name__ == "__main__":
     main()
+    
