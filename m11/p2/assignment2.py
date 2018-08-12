2f5a1d3308841c8ae2edd913352784c839619111
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-12 15:40:49 +0530

    " -> 6 of 6 passed. pylint: 9.41/10 "

--- m11/p2/assignment2.py
+++ m11/p2/assignment2.py
@@ -1,37 +1,20 @@
-#Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
-
-
-def updateHand(hand, word):
-    """
-    Assumes that 'hand' has all the letters in word.
-    In other words, this assumes that however many times
-    a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
-
-    Updates the hand: uses up the letters in the given word
-    and returns the new hand, without those letters in it.
-
-    Has no side effects: does not modify hand.
-
-    word: string
-    hand: dictionary (string -> int)    
-    returns: dictionary (string -> int)
-    """
-    # TO DO ... <-- Remove this comment when you code this function
-    
-
+"""Exercise: Assignment-2"""
+def update_hand(hand_1, word_1):
+    """a2"""
+    h_2 = hand_1
+    for i in word_1:
+        if i in h_2:
+            h_2[i] = h_2[i]-1
+    return h_2
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
-
-
-if __name__== "__main__":
-	main()
+    """string"""
+    n_1 = input()
+    adict_1 = {}
+    for i in range(int(n_1)):
+        data = input()
+        l_1 = data.split(" ")
+        adict_1[l_1[0]] = int(l_1[1])
+    data1 = input()
+    print(update_hand(adict_1, data1))
+if __name__ == "__main__":
+    main()
