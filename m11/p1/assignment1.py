ffe6947fe000379debd6b53c89a8eeb8261b8dd7
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-12 15:48:32 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- m11/p1/assignment1.py
+++ m11/p1/assignment1.py
@@ -11,19 +11,22 @@ def get_word_score(word_1, n_1):
     letters are used on the first turn.
 
     Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
-    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+    worth 3, D is worth 2, E is worth 1, and so on (see scrabble_word)
 
     word: string (lowercase letters)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
     # TO DO ... <-- Remove this comment when you code this function
-    SCRABBLE_LETTER_VALUES = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    scrabble_word = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1\
+, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3\
+, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1\
+, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     l_1 = len(word_1)
     score = 0
     for i in word_1:
-        if i in SCRABBLE_LETTER_VALUES.keys():
-            score += SCRABBLE_LETTER_VALUES[i]
+        if i in scrabble_word.keys():
+            score += scrabble_word[i]
     score = score*l_1
     if l_1 == n_1:
         score = 50 + score
