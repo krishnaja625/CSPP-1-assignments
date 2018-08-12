286d2e608c1bd949217d6ba63de7160d3e026083
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-12 16:30:05 +0530

    " -> 3 of 3 passed. pylint: -2.35/10 "

--- m11/p3/assignment3.py
+++ m11/p3/assignment3.py
@@ -20,23 +20,18 @@ def isValidWord(word, hand, wordList):
     """
     # TO DO ... <-- Remove this comment when you code this function
     for i in word:
-    	if i not in hand:
-    		return False
+        if i not in hand:
+            return False
     return word in wordList
-
-
 def main():
-	word=input()
-	n=int(input())
-	adict={}
-	for i in range(n):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	l2=input().split()
-	print(isValidWord(word,adict,l2))
-		
-
-
+    word=input()
+    n=int(input())
+    adict={}
+    for i in range(n):
+        data=input()
+        l=data.split()
+        adict[l[0]]=int(l[1])
+    l2=input().split()
+    print(isValidWord(word,adict,l2))
 if __name__== "__main__":
-	main()
+    main()
