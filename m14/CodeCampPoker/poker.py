bb00114d15983b7c21acaa78a0f72b80cd66cf4f
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-14 20:41:00 +0530

    " -> 3 of 3 passed. pylint: 8.37/10 "

--- m14/CodeCampPoker/poker.py
+++ m14/CodeCampPoker/poker.py
@@ -14,7 +14,7 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    dic = {'A':14,'2':2,'3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13}
+    dic = {'A':14, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13}
     list_1 = []
     cont = 0
     for card in hand:
