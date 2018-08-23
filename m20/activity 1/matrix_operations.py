4d60340df5a9c4ad5aff813ec7a5333b1caf9699
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-23 18:42:53 +0530

    " -> 7 of 8 passed. pylint: 6.53/10 "

--- m20/activity 1/matrix_operations.py
+++ m20/activity 1/matrix_operations.py
@@ -6,14 +6,14 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    if len(m_1[0]) == len(m_2):
+    if len(m1[0]) == len(m2):
         Result_1 = []
-        for i in range(len(m_1)):
+        for i in range(len(m1)):
             a_1 = []
-            for j in range(len(m_2[0])):
+            for j in range(len(m2[0])):
                 Res_1 = 0
-                for k in range(len(m_2)):
-                    Res_1 += m_1[i][k] * m_2[k][j]
+                for k in range(len(m2)):
+                    Res_1 += m1[i][k] * m2[k][j]
                 a_1.append(Res_1)
             Result_1.append(a_1)
         return Result_1
@@ -34,7 +34,7 @@ def add_matrix(m1, m2):
     if len(m1) != len(m2):
         print("Error: Matrix shapes invalid for addition")
         return None
-    for i,j in zip(m1,m2):
+    for i,j in zip(m1, m2):
         if len(i) != len(j):
             print("Error: Matrix shapes invalid for addition")
             return None
