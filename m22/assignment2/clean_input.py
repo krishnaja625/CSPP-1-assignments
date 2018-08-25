a2209e589e2d3e126ea58f468627ab9d9d5423af
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-25 10:44:45 +0530

    " -> 2 of 3 passed. pylint: -0.77/10 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -6,7 +6,7 @@ alphabets in both upper and lower case and numbers.
 def clean_string(string):
 	B = ""
 	for char in string:
-		if char in "!@#$%^&*":
+		if char in "!@#$%^&*' '()":
 			char = ""
 			B = B+char
 		else:
