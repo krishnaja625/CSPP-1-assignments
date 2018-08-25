148091ff4889e488344ea85d812dd29866c2ca1e
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-25 12:29:45 +0530

    " -> 4 of 26 passed. pylint: -3.02/10 "

--- m22/Check Sudoku/check_sudoku.py
+++ m22/Check Sudoku/check_sudoku.py
@@ -13,31 +13,32 @@ def check_sudoku(sudoku):
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
+    return solveSudoku(sudoku)
     
-    def findNextCellToFill(grid, i, j):
-        for x in range(i,9):
-            for y in range(j,9):
-                if grid[x][y] == 0:
-                    return x,y
-        for x in range(0,9):
-            for y in range(0,9):
-                if grid[x][y] == 0:
-                    return x,y
-        return -1,-1
+def findNextCellToFill(grid, i, j):
+    for x in range(i,9):
+        for y in range(j,9):
+            if grid[x][y] == 0:
+                return x,y
+    for x in range(0,9):
+        for y in range(0,9):
+            if grid[x][y] == 0:
+                return x,y
+    return -1,-1
 
-    def isValid(grid, i, j, e):
-            rowOk = all([e != grid[i][x] for x in range(9)])
-            if rowOk:
-                columnOk = all([e != grid[x][j] for x in range(9)])
-                if columnOk:
-                    # finding the top left x,y co-ordinates of the section containing the i,j cell
-                    secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
-                    for x in range(secTopX, secTopX+3):
-                        for y in range(secTopY, secTopY+3):
-                            if grid[x][y] == e:
-                                return False
-                    return True
-            return False
+def isValid(grid, i, j, e):
+        rowOk = all([e != grid[i][x] for x in range(9)])
+        if rowOk:
+            columnOk = all([e != grid[x][j] for x in range(9)])
+            if columnOk:
+                # finding the top left x,y co-ordinates of the section containing the i,j cell
+                secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
+                for x in range(secTopX, secTopX+3):
+                    for y in range(secTopY, secTopY+3):
+                        if grid[x][y] == e:
+                            return False
+                return True
+        return False
 def solveSudoku(grid, i=0, j=0):
         i,j = findNextCellToFill(grid, i, j)
         if i == -1:
