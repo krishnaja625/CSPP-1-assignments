780fb4c5e1bc931420ff59ae81a13e9b6967ae4c
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-25 12:17:46 +0530

    " -> 0 of 4 passed. pylint: 8.08/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -8,17 +8,16 @@ def tokenize(string):
     line_2 = []
     list_3 = []
     dict_1 = {}
-    C = 1
     for line in string:
         line_1 = line.split()
         line_2.append(line_1)
         for line in line_2:
             for word in line:
                 if word not in list_3:
-                    list_3.append(word)
+                    dict_1.keys().append(word)
+                    dict_1[word] += line.count(word) 
                 else:
-                    C += line.count(word) 
-    dict_1 = dict(zip(list_3, C))
+                    dict_1[word] += line.count(word)  
     return dict_1
 def main():
     string = ''
