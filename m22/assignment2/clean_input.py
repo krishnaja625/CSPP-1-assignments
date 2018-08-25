005f5c352fe8fbc0c9f8d1a0533c211ecd5c70aa
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-25 10:41:43 +0530

    " -> 0 of 3 passed. pylint: -0.77/10 "

---
+++ m22/assignment2/clean_input.py
@@ -0,0 +1,21 @@
+'''
+Write a function to clean up a given string by removing the special characters and retain 
+alphabets in both upper and lower case and numbers.
+'''
+
+def clean_string(string):
+	B = ""
+	for char in string:
+		if char in "!@#$%^&*":
+			char = ""
+			B = B+char
+		else:
+			B = B+char
+	print(B)
+
+def main():
+    string = input()
+    print(clean_string(string))
+
+if __name__ == '__main__':
+    main()
