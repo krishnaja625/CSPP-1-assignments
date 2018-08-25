7557d13ebd38ea4571e617bc0d2711404f4a3629
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-25 12:47:39 +0530

    " -> 0 of 4 passed. pylint: -3.89/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -4,21 +4,33 @@ each word
 '''
 
 def tokenize(string):
-    line_1 = []
-    line_2 = []
-    list_3 = []
-    dict_1 = {}
-    for line in string:
-        line_1 = line.split()
-        line_2.append(line_1)
-        for line in line_2:
-            for word in line:
-                if word not in list_3:
-                    dict_1.append(word)
-                    dict_1[word] += line.count(word) 
-                else:
-                    dict_1[word] += line.count(word)  
-    return dict_1
+    # line_1 = []
+    # line_2 = []
+    # list_3 = []
+    # dict_1 = {}
+    # for line in string:
+    #     line_1 = line.split()
+    #     line_2.append(line_1)
+    #     for line in line_2:
+    #         for word in line:
+    #             if word not in list_3:
+    #                 dict_1.append(word)
+    #                 dict_1[word] += line.count(word) 
+    #             else:
+    #                 dict_1[word] += line.count(word)  
+    # return dict_1
+    lines = [line.split() for line in string.split('\n')]
+
+    for i in range(len(lines)):
+        for word in lines[i]:
+            # if the word is not in the dictionary, create the entry
+            # word = word.lower()
+            if word not in words:
+                words[word] = {'count':0, 'lines':set()}
+
+            # update the count and add the line number to the set
+            words[word]['count'] += 1
+            words[word]['lines'].add(i+1)
 def main():
     string = ''
     lines = int(input())
@@ -26,8 +38,8 @@ def main():
         i += 1
         string += input()
         string += '\n'
-    line = string.splitlines()
-    print(tokenize(line))
+    # line = string.splitlines()
+    print(tokenize(string))
 
 if __name__ == '__main__':
     main()
